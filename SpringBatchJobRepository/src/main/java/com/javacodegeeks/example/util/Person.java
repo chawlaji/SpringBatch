4516bbf92e3fe130c66a6905b3aa4873033039d2
Gaurav Garg <30888086+ggarg0@users.noreply.github.com>
2018-01-03 17:30:46 +0530

    Commit
    Commit

---
+++ SpringBatchJobRepository/src/main/java/com/javacodegeeks/example/util/Person.java
@@ -0,0 +1,41 @@
+package com.javacodegeeks.example.util;
+
+public class Person {
+   String firstName,lastName,school;
+   int rollNumber; 
+     
+   
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	
+	public int getRollNumber() {
+		return rollNumber;
+	}
+	public void setRollNumber(int rollNumber) {
+		this.rollNumber = rollNumber;
+	}	
+  
+	@Override
+	public String toString(){
+		return "Welcome, "+ firstName+" "+ lastName+" to "+ school+"!";
+	}
+
+}
